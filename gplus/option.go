7c95f2dc2a8dfb64fe0408a3b05ab04997d1ac46
zouchangfu <zcf13543354971@163.com>
2023-05-20 22:29:49 +0800

    feat: add selectPage ignore total #37

--- gplus/option.go
+++ gplus/option.go
@@ -57,7 +57,7 @@ func Omit(columns ...any) OptionFunc {
 	}
 }
 
-// IgnoreTotal 分页查询忽略总数
+// IgnoreTotal 分页查询忽略总数 issue: https://github.com/acmestack/gorm-plus/issues/37
 func IgnoreTotal() OptionFunc {
 	return func(o *Option) {
 		o.IgnoreTotal = true
